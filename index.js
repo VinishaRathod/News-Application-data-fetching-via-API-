466410f3978fb1c277e064b69eb865e29fe3de38
VinishaRathod <er.vinisharathod@gmail.com>
2024-05-06 23:16:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+        // 
+        //   data.map((item, index) => {
+        //     document.write(
+        //       `<p>Name: ${item.name}<br/>Email:${item.email}</p>` +
+        //         ` <button onclick='deleteUser(${item.id})'>Delete User</button><br/>`
+        //     );
+        //   });
+        // });
